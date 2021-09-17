7b6a9e10efa21c74714ad7a1f507fdb4befec798
Victor Hugo Moschini Santos <victor.moschini@gmail.com>
2021-09-16 23:01:40 -0300

    Alterações no script

--- Assets/Scripts/Mechanic/VisionWords.cs
+++ Assets/Scripts/Mechanic/VisionWords.cs
@@ -7,21 +7,40 @@ public class VisionWords : MonoBehaviour
 {
     [SerializeField] private float range;
     [SerializeField] private float rightAngle;
+
     public UnityEvent FindWord;
+
+    private float holdTime = 1;
+    private IEnumerator coroutine;
+
+    private void Start()
+    {
+        coroutine = HoldCooldown(holdTime);
+    }
 
     private void OnTriggerStay(Collider other)
     {
-        // Enquanto o jogador estiver dentro do colisor e olhando dentro da folga de angulo, invoca o trigger
         if (other.tag == "Player")
         {
             var actualRotation = other.transform.eulerAngles.y;
-            Debug.Log(actualRotation);
-            //Debug.Log("Range: " + (rightAngle - range) + " - " + (rightAngle + range));
 
             if (actualRotation > rightAngle - range && actualRotation < rightAngle + range)
             {
-                FindWord.Invoke();
+                StartCoroutine(coroutine);
             }
         }
     }
+
+    IEnumerator HoldCooldown(float _holdTime)
+    {
+        yield return new WaitForSeconds(_holdTime);
+
+        FindWord.Invoke();
+    }
 }
+
+/*
+ * TODO
+ Comparar a distancia do jogador com esse objeto, e pegar a diferença e criar um glow que aumenta quanto mais perto
+o jogador estiver
+ */

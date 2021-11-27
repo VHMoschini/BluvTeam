2ebcca267041ac17ee3fc16222ae51dbbaa0ea61
Victor Hugo Moschini Santos <victor.moschini@gmail.com>
2021-11-27 20:49:26 -0300

    Sistema de falas e legenda

--- Assets/SCRIPT/Scripts/FMOD/SpeakAndSubtittleController.cs
+++ Assets/SCRIPT/Scripts/FMOD/SpeakAndSubtittleController.cs
@@ -1,110 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SpeakAndSubtittleController : MonoBehaviour
 {
-    public GameObject Subtittle;     //Objeto do canvas com a legenda da fala atual. Se for possível fazer uma parte do script pra dar um fade in e fade out é um polimento legal de ter
-
-    [SerializeField]
-    private FMODUnity.StudioEventEmitter VoiceLine;  //Studio Event Emmiter que contém a fala
-
-    public GameObject NextLine;  // Próximo gameobject que contém fala
-
-    /*
-    public GameObject ObjectToActivate; 
-
-     Isso tava aqui pra ativar/desativar algum game object vinculado, se o Vitão ou alguém conseguir fazer a boa de colocar o mesmo esquema dos triggers
-    pra fazer esse script puxar outras interações eu pago 01 pacotinho de bala de menta Garoto :D
-    */
 
     public bool startAway = false;   //Faz a fala ativar no start do objeto
-    public bool isEnding = false;    //Marca a fala atual como a última fila/sequência, mesmo se tiverem outras vinculadas elas não irão tocar quando a atual terminar
-
-    //public bool enableSomething = false;
-    //public bool disableSomething = false;
+    public TMP_Text sub;
 
     private bool isActive = false;
-    private bool ended = false;
+    private int current_index = 0;
+
+    private FadeInWords wordControler;
+
+    [Space(10)]
+    [SerializeField] public List<AudioConfig> AUDIO_CONFIG = new List<AudioConfig>();
 
     void Start()
     {
-        if (startAway == true) StartPlay();
+        wordControler = sub.gameObject.GetComponent<FadeInWords>();
 
-       /* if(enableSomething == true || disableSomething == true)
+        foreach (AudioConfig i in AUDIO_CONFIG)
         {
-            if(ObjectToActivate == null)
-            {
-                Debug.Log("This script will try to enable or disable an object but no reference was found");
-            }
-        } */
+            i.audioEvent = FMODUnity.RuntimeManager.CreateInstance(i.VoiceLine);
+        }
+
+        if (startAway == true) StartPlay(current_index);
     }
 
     void Update()
     {
-        if(isActive == true && ended == false) //Verifica se o evento foi ativado e se não foi tocado ainda
+        AudioConfig i = AUDIO_CONFIG[current_index];
+
+        FMOD.Studio.PLAYBACK_STATE actualState;
+        i.audioEvent.getPlaybackState(out actualState);
+
+        if (isActive == true && actualState == FMOD.Studio.PLAYBACK_STATE.STOPPED)
         {
-            if(VoiceLine.IsPlaying() == false)
+            if (current_index < AUDIO_CONFIG.Count - 1)
             {
-                if (isEnding == true) //Verifica se o evento é o último da fila e finaliza a execução
-                {
-                    Debug.Log("Queue ended, last event played was " + VoiceLine.gameObject.name);
-                    ended = true;
+                i.OnEnd.Invoke();
 
-                    DisableSelf();
-                }
-                else if (NextLine == null) //Verifica se nenhum objeto foi vinculado como próximo da fila e finaliza a execução
-                {
-                    Debug.Log("Queue stopped, no next line was found");
-                    ended = true;
+                current_index++;
+                StartPlay(current_index);
+            }
+            else
+            {
+                isActive = false;
+                current_index = 0;
 
-                    DisableSelf();
-                }
-                else StartNext(); //Inicia o próximo evento da fila
-
+                wordControler.FadeOut();
             }
         }
     }
 
-    public void StartPlay() //Comanda a ativação do audio e da legenda
+    public void StartPlay(int index)
     {
-        if(ended == false)
-        {
-            if (VoiceLine.IsPlaying() != true && isActive == false) //Verifica se o evento já está tocando antes de iniciar
-            {
-                VoiceLine.Play();
-                isActive = true;
-                Subtittle.SetActive(true);
+        AudioConfig i = AUDIO_CONFIG[index];
 
-                Debug.Log("Playing " + VoiceLine.gameObject.name);
-            }
+        i.audioEvent.start();
 
-            /* if (enableSomething == true)
-            {
-                ObjectToActivate.SetActive(true);
-            } */
-        } else Debug.Log("The line " + VoiceLine.gameObject.name + " is playing or was already played once, check objects references");
+        sub.text = i.Subtitle;
+        if (!isActive) wordControler.FadeIn();
 
+        isActive = true;
     }
+}
 
-    void StartNext() //Toca o próximo evento da fila
-    {
-        Debug.Log("Started next line " + NextLine.name);
-        NextLine.GetComponent<SpeakAndSubtittleController>().StartPlay();
+[System.Serializable]
+public class AudioConfig
+{
+    [SerializeField]
+    [FMODUnity.EventRef] public string VoiceLine = "event:/Dublagem";
 
-        ended = true;
-        DisableSelf();
-    }
+    [HideInInspector]
+    public FMOD.Studio.EventInstance audioEvent;
 
-    void DisableSelf() //Desabilita a legenda atual, acho legal colocar um timer de 1 ou 2 segundos para desabilitar mas não tive tempo de fazer
-    {
-        /* if(disableSomething == true)
-        {
-            ObjectToActivate.SetActive(false);
-        }
-        */
+    [TextArea] [SerializeField] public string Subtitle;
 
-        Subtittle.SetActive(false);
-        
-    }
+    [Space(10)]
+    [SerializeField] public UnityEvent OnEnd;
 }
--- Assets/SCRIPT/System/Palavras/FadeInWords.cs
+++ Assets/SCRIPT/System/Palavras/FadeInWords.cs
@@ -5,20 +5,41 @@ using UnityEngine;
 
 public class FadeInWords : MonoBehaviour
 {
-    private TextMeshPro word;
+    private TMP_Text word;
+
+    public bool OnStart = true;
+
+    private void Start()
+    {
+        word = GetComponent<TMP_Text>();
+        
+    }
 
     void OnEnable()
     {
-        word = GetComponent<TextMeshPro>();
+        if (!OnStart) return;
+
         word.color = new Color(255, 255, 255, 0);
-        StartCoroutine(fadeIn());
+        StartCoroutine(fadeIn_coroutine());
     }
 
-    IEnumerator fadeIn()
+    IEnumerator fadeIn_coroutine()
     {
         while (word.color.a <= 1)
         {
             word.color = new Color(255, 255, 255, word.color.a + 0.05f);
+            yield return new WaitForSeconds(0.03f);
+        }
+    }
+
+    public void FadeIn() => StartCoroutine(fadeIn_coroutine());
+    public void FadeOut() => StartCoroutine(fadeOut_coroutine());
+
+    IEnumerator fadeOut_coroutine()
+    {
+        while (word.color.a >= 0)
+        {
+            word.color = new Color(255, 255, 255, word.color.a - 0.05f);
             yield return new WaitForSeconds(0.03f);
         }
     }
